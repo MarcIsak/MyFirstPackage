7bbc426079b173ab0fcab5be3ad91ec4c6bfa53d
MarcIsak <marc.isaksson89@gmail.com>
2019-01-30 11:06:23 +0100

    "this is another message"

--- R/makeheatmap.R
+++ R/makeheatmap.R
@@ -2,6 +2,6 @@
 #'@param sco input variable gene matrix
 #'@export makeheatmap
 makeheatmap <- function(sco){
-  pheatmap(sco@data[sco@var.genes,],scale="row") #this is a comment
+  pheatmap(sco@data[sco@var.genes,],scale="row") #this is another comments
 
 }
